c8e851f87690a6a48d8a6762400a618c98e0c3a2
Qianying Ruan <124208512+1239874569898@users.noreply.github.com>
2023-10-22 22:09:59 -0700

    Add files via upload

---
+++ new-item.js
@@ -0,0 +1,10 @@
+"use client";
+
+import { useState } from "react";
+
+export default function Newitem() {
+    const [name, setName] = useState("");
+    const [quantity, setQuantity] = useState(1);
+    const [category, setCategory] = useState(product);
+}
+
---
+++ page.js
@@ -0,0 +1,9 @@
+import Newitem from "./new-item";
+
+export default function Page() {
+  return (
+    <main>
+      <Newitem />
+    </main>
+  );
+}
